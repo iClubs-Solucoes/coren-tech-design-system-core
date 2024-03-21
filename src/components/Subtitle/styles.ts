a9667f9449aec56598d08800688d5d2f07c2514b
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-03-21 16:57:35 -0300

    change font weight on subtitle container

--- src/components/Subtitle/styles.ts
+++ src/components/Subtitle/styles.ts
@@ -2,6 +2,6 @@ import { colors } from 'common/styles';
 import styled from 'styled-components';
 
 export const SubtitleContainer = styled.h2`
-  font: 200 1.6rem Inter;
+  font: 300 1.6rem Inter;
   color: ${colors.normal.black};
 `;
