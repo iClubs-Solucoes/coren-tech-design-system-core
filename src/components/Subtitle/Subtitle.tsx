8dcb8a95bede8097e2aae4fe231a2c22f3bb0c60
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-03-21 15:41:37 -0300

    add className to subtitle component

--- src/components/Subtitle/Subtitle.tsx
+++ src/components/Subtitle/Subtitle.tsx
@@ -1,6 +1,8 @@
 import * as S from './styles';
 import { SubtitleProps } from './types';
 
-export function Subtitle({ children }: SubtitleProps) {
-  return <S.SubtitleContainer>{children}</S.SubtitleContainer>;
+export function Subtitle({ children, className }: SubtitleProps) {
+  return (
+    <S.SubtitleContainer className={className}>{children}</S.SubtitleContainer>
+  );
 }
--- src/components/Subtitle/types.ts
+++ src/components/Subtitle/types.ts
@@ -1,3 +1,4 @@
 export type SubtitleProps = {
   children: string;
+  className: string;
 };
