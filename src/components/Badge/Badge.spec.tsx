047d86dbdf004fb6f6148fab0fed63ec0f605bcc
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-03-25 17:20:44 -0300

    add unit tests to badge

--- src/components/Badge/Badge.spec.tsx
+++ src/components/Badge/Badge.spec.tsx
@@ -4,9 +4,46 @@ import { describe, expect, it } from 'vitest';
 import { Badge } from './Badge';
 
 describe('<Badge />', () => {
-  it('should render the component', () => {
-    render(<Badge>Test</Badge>);
+  const children = 'Badge';
+  const title = 'Title';
 
-    expect(screen.getByText(/Test/i)).toBeTruthy();
+  it('should render the component', () => {
+    render(
+      <Badge className="123" title={title}>
+        {children}
+      </Badge>,
+    );
+
+    const badge = screen.getByText(new RegExp(children, 'i'));
+
+    expect(badge).toBeTruthy();
+  });
+
+  it('should have title property', () => {
+    render(<Badge title={title}>{children}</Badge>);
+    const badge = screen.getByText(new RegExp(children, 'i'));
+
+    expect(badge).toHaveProperty('title', title);
+  });
+
+  it('should render without children', () => {
+    render(<Badge title={title} />);
+    const badge = screen.getByTitle(new RegExp(title, 'i'));
+
+    expect(badge).toBeTruthy();
+  });
+
+  it('should render without title', () => {
+    render(<Badge>{children}</Badge>);
+    const badge = screen.getByText(new RegExp(children, 'i'));
+
+    expect(badge).toBeTruthy();
+  });
+
+  it('should forward className to the DOM element', () => {
+    render(<Badge className="123">{children}</Badge>);
+    const badge = screen.getByText(new RegExp(children, 'i'));
+
+    expect(badge.className).includes('123');
   });
 });
