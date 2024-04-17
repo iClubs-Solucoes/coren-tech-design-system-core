160e8623f7a6aab4d48fd32b27cbc0f0181caa86
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-04-17 14:56:47 -0300

    add navbar's unit test

--- src/primitives/components/Navbar/Navbar.spec.tsx
+++ src/primitives/components/Navbar/Navbar.spec.tsx
@@ -3,15 +3,28 @@ import { ScrollProvider } from 'hooks';
 import { describe, expect, it } from 'vitest';
 
 import { Navbar } from './Navbar';
+import { NavbarProps } from './types';
+
+function renderComponent(props?: NavbarProps) {
+  return render(
+    <ScrollProvider>
+      <Navbar {...props}>Test</Navbar>
+    </ScrollProvider>,
+  );
+}
 
 describe('<Navbar />', () => {
   it('should render the component', () => {
-    render(
-      <ScrollProvider>
-        <Navbar>Test</Navbar>
-      </ScrollProvider>,
-    );
+    renderComponent();
 
     expect(screen.getByText(/Test/i)).toBeTruthy();
+  });
+
+  it('should forward className to the DOM element', () => {
+    renderComponent({ className: '123' });
+
+    const navbar = screen.getByText(/Test/i);
+
+    expect(navbar.className).includes('123');
   });
 });
