ca2d878f2ad0117885aa969cd49572769fb225d1
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-04-26 13:36:54 -0300

    add visualization details features

---
+++ src/components/DatePicker/styles.ts
@@ -0,0 +1,6 @@
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import styled from 'dynamic-styled-components';
+
+export const DatePickerContainer = styled(DatePicker)``;
