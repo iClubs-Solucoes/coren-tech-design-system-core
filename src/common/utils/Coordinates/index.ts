7f3dec73dba943b98c629d7f2bbcfa421ea5264d
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-06-14 14:03:27 -0300

    add doesNotOverflowScreen

--- src/common/utils/Coordinates/index.ts
+++ src/common/utils/Coordinates/index.ts
@@ -3,3 +3,5 @@ export { coordinatesComponent } from './coordinatesComponent';
 export { openAboveOrBelow } from './openAboveOrBelow';
 
 export { getDistanceFromBordersInsights } from './getDistanceFromBordersInsights';
+
+export { doesNotOverflowScreen } from './doesNotOverflowScreen';
